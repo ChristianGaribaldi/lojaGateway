db2f3201cb2293c3b0fb7eae227df4d87e0851e5
christian <christiangaribaldi@hotmail.com>
2016-10-15 13:16:51 -0300

    * Correção ClienteMicroservico

--- app/models/cliente_microservico.rb
+++ app/models/cliente_microservico.rb
@@ -13,15 +13,33 @@ class ClienteMicroservico < Microservicos
   end
 
   def apagar_cliente(id_recurso)
-    return resposta = apagar_recurso(id_recurso)
+    resposta = apagar_recurso(id_recurso)
+    # Código NO_CONTENT= 204
+    if resposta.code = 204
+      true
+    else
+      false
+    end
   end
 
   def criar_cliente(novo_recurso)
     resposta = criar_recurso({:cliente => novo_recurso})
+    # Código CREATED = 201
+    if resposta.code = 201
+      true
+    else
+      false
+    end
   end
 
   def atualizar_cliente(recurso)
     resposta = atualizar_recurso(recurso[:id], {:cliente => recurso})
+    # Código NO_CONTENT= 204
+    if resposta.code = 204
+      true
+    else
+      false
+    end
   end
 
   def trata_resposta_cliente(resposta)
